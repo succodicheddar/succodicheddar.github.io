4dc9403874405dd99fa7bba130836448122058bd
succodicheddar <sukhjotsingh.jaswal@cupparisalvati.edu.it>
2025-05-09 13:00:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function coccolami() {
   // Mostra testo e seconda immagine
   document.getElementById("magiaText").style.display = "block";
   document.getElementById("secondaFotoContainer").style.display = "block";
-
+  
+function scintille() {
+  const container = document.querySelector(".gatto-container");
+  const sparkle = document.createElement("div");
+  sparkle.className = "scintilla";
+  sparkle.style.left = Math.random() * 300 + "px";
+  sparkle.style.top = Math.random() * 300 + "px";
+  container.appendChild(sparkle);
+  setTimeout(() => sparkle.remove(), 1000);
+}
+setInterval(scintille, 1000);
   // Cuori
   const container = document.getElementById("cuoriContainer");
   const width = container.offsetWidth;
